291ca66ae5e6e94813fd0c605ffbb078e71e783d
Alina <sobolevaalinaa@yandex.ru>
2023-10-14 20:36:50 +0300

    style fix

--- stats/stats-server/src/main/java/ru/practicum/statsserver/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/statsserver/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.statsserver;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,9 +13,9 @@ import java.time.format.DateTimeParseException;
 @Slf4j
 public class ErrorHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler({DataIntegrityViolationException.class, DateTimeParseException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleDateTimeParseException(final DateTimeParseException e) {
+    public ErrorResponse handleDateTimeParseException(final Exception e) {
         log.debug("Получен статус 400 Bad request: {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
